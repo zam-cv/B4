464db2f920c5d0ca7d1f1075210ad8e003a4bb8b
zam-cv <a01799283@tec.mx>
2024-02-21 00:12:16 -0600

    add: bar

--- platform/src/App.tsx
+++ platform/src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline p-10">Home</h1>;
+  return <h1 className="text-3xl font-bold underline p-20">Home</h1>;
 }
--- platform/src/main.tsx
+++ platform/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <div data-tauri-drag-region className="bar"></div>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
