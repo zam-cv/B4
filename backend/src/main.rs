ff46ed09879cf17da4522035391d4533445e5981
zam-cv <a01799283@tec.mx>
2024-02-21 15:35:25 -0600

    add: static files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files as fs;
+use actix_web::{App, HttpServer};
+
+pub const HOST: &str = "0.0.0.0";
+pub const PORT: &str = "8080";
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running at {}:{}", HOST, PORT);
+    HttpServer::new(|| {
+        App::new().service(
+            fs::Files::new("/", "../page/")
+                .show_files_listing()
+                .index_file("index.html"),
+        )
+    })
+    .bind(format!("{}:{}", HOST, PORT))?
+    .run()
+    .await
 }
