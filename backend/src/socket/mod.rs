15ad80ba04c762106658f2260727582b6f2e7fe0
zam-cv <a01799283@tec.mx>
2024-02-25 16:23:39 -0600

    socket reorganization

---
+++ backend/src/socket/mod.rs
@@ -0,0 +1,22 @@
+use actix::prelude::*;
+use actix_web::{web, Error, HttpMessage, HttpRequest, HttpResponse};
+use actix_web_actors::ws;
+
+pub mod server;
+pub mod session;
+
+pub async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    // The id was obtained from the token when authenticating
+    if let Some(id) = req.extensions().get::<i32>() {
+        return ws::start(
+            session::Session {
+                id: *id,
+                addr: server::Server::new().start(),
+            },
+            &req,
+            stream,
+        );
+    }
+
+    Ok(HttpResponse::Unauthorized().finish())
+}
---
+++ backend/src/socket/server.rs
@@ -0,0 +1,65 @@
+use crate::socket::session::{Message, Response, Session};
+use actix::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Connect {
+    pub id: i32,
+    pub addr: Addr<Session>,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub id: i32,
+}
+
+pub struct Server {
+    sessions: HashMap<i32, Addr<Session>>,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Server {
+            sessions: HashMap::new(),
+        }
+    }
+}
+
+impl Actor for Server {
+    type Context = Context<Self>;
+}
+
+/// This handler is responsible for processing the `Connect` message and generating a response.
+impl Handler<Connect> for Server {
+    type Result = ();
+
+    fn handle(&mut self, conn: Connect, _: &mut Self::Context) -> Self::Result {
+        println!("Connected: {}", conn.id);
+        self.sessions.insert(conn.id, conn.addr);
+    }
+}
+
+/// This allows the `Server` actor to react to disconnection events and perform any necessary cleanup or logging.
+impl Handler<Disconnect> for Server {
+    type Result = ();
+
+    fn handle(&mut self, disc: Disconnect, _: &mut Self::Context) {
+        println!("Disconnected: {}", disc.id);
+        self.sessions.remove(&disc.id);
+    }
+}
+
+/// This implementation defines how the `Server` actor should handle incoming messages of type `Message`.
+impl Handler<Message> for Server {
+    type Result = ();
+
+    fn handle(&mut self, msg: Message, _: &mut Self::Context) {
+        println!("Message from {}: {}", msg.0, msg.1);
+
+        if let Some(addr) = self.sessions.get(&msg.0) {
+            addr.do_send(Response("Response".to_string()));
+        }
+    }
+}
