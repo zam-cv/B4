534c227513c1607886e44eacc9bc42209b2896a4
zam-cv <a01799283@tec.mx>
2024-04-12 12:51:58 -0600

    update: bank

---
+++ backend/src/bank/sentences.rs
@@ -0,0 +1,141 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref FUNCTION_REGEX: Regex = Regex::new(r"^([A-Za-x].*)\((.*)\)$").unwrap();
+    static ref VARIABLES_REGEX: Regex = Regex::new(r"\$\{.*?\}").unwrap();
+}
+
+#[derive(Deserialize)]
+pub struct RawSentence {
+    pub getters: HashMap<&'static str, &'static str>,
+    pub value: &'static str,
+    pub handlers: Vec<&'static str>,
+}
+
+#[derive(Deserialize)]
+#[serde(bound(deserialize = "'de: 'static"))]
+pub struct RawSentences {
+    pub positive: Vec<RawSentence>,
+    pub negative: Vec<RawSentence>,
+    pub default: Vec<RawSentence>,
+    pub tips: Vec<&'static str>,
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: &'static str,
+    pub args: Vec<&'static str>,
+}
+
+#[derive(Debug)]
+pub enum SentenceFragment {
+    Variable(&'static str),
+    Text(&'static str),
+}
+
+#[derive(Debug)]
+pub struct Sentence {
+    pub getters: HashMap<&'static str, Function>,
+    pub value: Vec<SentenceFragment>,
+    pub handlers: Vec<Function>,
+}
+
+#[derive(Debug)]
+pub struct Sentences {
+    pub positive: Vec<Sentence>,
+    pub negative: Vec<Sentence>,
+    pub default: Vec<Sentence>,
+    pub tips: Vec<&'static str>,
+}
+
+fn get_function(function: &'static str) -> Function {
+    let captures = FUNCTION_REGEX.captures(function).unwrap();
+    let name = captures.get(1).map_or("", |m| m.as_str());
+    let args: Vec<&str> = captures
+        .get(2)
+        .map_or("", |m| m.as_str())
+        .split(',')
+        .map(|arg| arg.trim())
+        .collect();
+
+    Function { name, args }
+}
+
+fn raw_sentence_to_sentence(raw: RawSentence) -> Sentence {
+    let getters = raw
+        .getters
+        .into_iter()
+        .map(|(key, value)| (key, get_function(value)))
+        .collect();
+
+    let handlers = raw.handlers.into_iter().map(get_function).collect();
+    let mut value = Vec::new();
+
+    // finds the positions of the variables and cuts the text
+    let mut last = 0;
+    for captures in VARIABLES_REGEX.captures_iter(raw.value) {
+        let start = captures.get(0).unwrap().start();
+        let end = captures.get(0).unwrap().end();
+
+        if start > last {
+            value.push(SentenceFragment::Text(&raw.value[last..start]));
+        }
+
+        value.push(SentenceFragment::Variable(&raw.value[start + 2..end - 1]));
+        last = end;
+    }
+
+    if last < raw.value.len() {
+        value.push(SentenceFragment::Text(&raw.value[last..]));
+    }
+
+    Sentence {
+        getters,
+        value,
+        handlers,
+    }
+}
+
+pub fn raw_sentences_to_sentences(raw: RawSentences) -> Sentences {
+    let positive: Vec<Sentence> = raw
+        .positive
+        .into_iter()
+        .map(raw_sentence_to_sentence)
+        .collect();
+
+    let negative: Vec<Sentence> = raw
+        .negative
+        .into_iter()
+        .map(raw_sentence_to_sentence)
+        .collect();
+
+    let default: Vec<Sentence> = raw
+        .default
+        .into_iter()
+        .map(raw_sentence_to_sentence)
+        .collect();
+
+    Sentences {
+        positive,
+        negative,
+        default,
+        tips: raw.tips,
+    }
+}
+
+macro_rules! build_function_map {
+    ($ty:ty ,$module:ident, $($name:ident),*) => {
+        {
+            let mut map: HashMap<&'static str, $ty> = std::collections::HashMap::new();
+            $(
+                map.insert(stringify!($name), $module::$name);
+            )*
+            map
+        }
+    };
+}
+
+pub(crate) use build_function_map;
