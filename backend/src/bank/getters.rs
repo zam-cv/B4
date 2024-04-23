5bf8fc51353a9779248a51fa7abf8d57be847219
celiphcephei <ampheriz0@gmail.com>
2024-04-23 14:38:30 -0600

    add: getters

--- backend/src/bank/getters.rs
+++ backend/src/bank/getters.rs
@@ -3,6 +3,10 @@ use anyhow::Result;
 use macros::getter;
 use rand::Rng;
 
+fn get_random(first: u32, second: u32) -> u32 {
+    rand::thread_rng().gen_range(first..=second)
+}
+
 #[getter]
 fn __get_money(ctx: &mut Context) -> Result<String> {
     Ok(ctx.player.balance_cash.to_string())
@@ -10,5 +14,20 @@ fn __get_money(ctx: &mut Context) -> Result<String> {
 
 #[getter]
 fn __get_value_random(_: &mut Context, first: u32, second: u32) -> Result<String> {
-    Ok(rand::thread_rng().gen_range(first..second).to_string())
+    Ok(get_random(first, second).to_string())
+}
+
+#[getter]
+fn __robar(ctx: &mut Context) -> Result<String> {
+    let porcentaje: u32 = get_random(1, 100);
+    let cantidad_robada = (ctx.player.balance_cash as f64 * (porcentaje as f64 / 100.0)) as u32;
+    ctx.player.balance_cash -= cantidad_robada as i32;
+    Ok(cantidad_robada.to_string())
+}
+
+#[getter]
+fn __premio_mayor(ctx: &mut Context) -> Result<String> {
+    let ganancia = ctx.player.max_change / 100;
+    ctx.player.balance_cash += ganancia as i32;
+    Ok(ganancia.to_string())
 }
