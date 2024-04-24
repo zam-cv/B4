59be44a8b850a47f59ee3d91aff8e55b7f4d9ade
celiphcephei <ampheriz0@gmail.com>
2024-04-23 21:20:56 -0600

    add: serializing

--- backend/src/models/events.rs
+++ backend/src/models/events.rs
@@ -2,13 +2,16 @@ use std::hash::Hash;
 
 use crate::{models::{types::*, Statistic}, schema};
 use diesel::prelude::*;
+use utoipa::ToSchema;
+use serde::Serialize;
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, ToSchema, Serialize)]
 #[derive(Queryable, Selectable, Identifiable, Insertable, AsChangeset)]
 #[diesel(check_for_backend(diesel::mysql::Mysql))]
 #[diesel(primary_key(id))]
 #[diesel(table_name = schema::events)]
 pub struct Event {
+    #[serde(skip_serializing)]
     #[diesel(deserialize_as = i32)]
     pub id: Option<i32>,
     pub event_type: EventType,
