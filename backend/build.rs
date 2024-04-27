3d01464695114ee3da09a3b8b25bf4d6381e7b99
zam-cv <a01799283@tec.mx>
2024-04-27 11:39:26 -0600

    update: dockerfile

--- backend/build.rs
+++ backend/build.rs
@@ -3,6 +3,6 @@ use std::env;
 
 fn main() {
     dotenv().ok();
-    let mode = env::var("MODE").expect("MY_CONFIG must be set");
+    let mode = env::var("MODE").expect("MODE must be set");
     println!("cargo:rustc-cfg=feature=\"{}\"", mode);
 }
