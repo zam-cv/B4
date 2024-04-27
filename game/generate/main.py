cb1735a28ab707e12ddfdf6cb67b2d5ebfa58068
zam-cv <a01799283@tec.mx>
2024-04-26 18:07:09 -0600

    update: generate

--- game/generate/main.py
+++ game/generate/main.py
@@ -11,6 +11,7 @@ OUTPUT = os.path.join(PATH, "..", "..", "page", 'index.html')
 BUILD = os.path.join(PATH, "..", "..", "page", "Build")
 LOGO = os.path.join(PATH, "..", "..", "page", "verqor.png")
 BACKGROUND = os.path.join(PATH, "..", "..", "page", "background.png")
+V = os.path.join(PATH, "..", "..", "page", "v.png")
 
 with open(FILE, 'r') as f:
     with open(OUTPUT, 'w') as o:
@@ -32,3 +33,4 @@ with open(FILE, 'r') as f:
 
 shutil.copy(os.path.join(PATH, "verqor.png"), LOGO)
 shutil.copy(os.path.join(PATH, "background.png"), BACKGROUND)
+shutil.copy(os.path.join(PATH, "v.png"), V)
