f523d35610ed8b9f333f0b42db4fa813da2a1b06
celiphcephei <ampheriz0@gmail.com>
2024-05-04 22:16:12 -0600

    add: comments

--- game/Qrops/Assets/Scripts/EventsPanel.cs
+++ game/Qrops/Assets/Scripts/EventsPanel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/* 
+ * Esta clase se encarga de mostrar y ocultar un panel de eventos
+ * cuando se hace clic en un botón.
+ */
+
 public class EventsPanel : MonoBehaviour
 {
     public GameObject panel;
-
+    
+    // Esta función se llama cuando se hace clic en el botón
+    // Muestra el panel de eventos
     public void HidePanel()
     {
         panel.SetActive(false);
