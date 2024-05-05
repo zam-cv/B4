736e1d549d15260a27b44af2bca669a130c35512
celiphcephei <ampheriz0@gmail.com>
2024-05-04 22:16:45 -0600

    add: comments

--- game/Qrops/Assets/Scripts/ParcelaActual.cs
+++ game/Qrops/Assets/Scripts/ParcelaActual.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ * Esta clase se encarga de manejar la parcela actual en la que se encuentra
+ * el jugador.
+ */
+
 public class ParcelaActual : MonoBehaviour
 {
     //Crea una variable publica la cual se pueda acceder desde otro script. Debe ser integer y llamarce NumeroParcela
     public int NumeroParcela;
     public static ParcelaActual instance;
 
+    // Función que se llama al inicio del juego y asigna la instancia de esta clase
     void Start()
     {
         //Si la instancia es nula, se le asigna el valor de esta clase
@@ -16,6 +22,7 @@ public class ParcelaActual : MonoBehaviour
         }
     }
     
+    // Se llama cuando se hace clic en la parcela, sirve para debuggear
     void OnMouseDown(){
         print(NumeroParcela);
     }
