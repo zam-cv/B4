769bb167a1bc7ecd652c3814076a52610e6431dd
celiphcephei <ampheriz0@gmail.com>
2024-05-04 22:16:33 -0600

    add: comments

--- game/Qrops/Assets/Scripts/MusicaFondo.cs
+++ game/Qrops/Assets/Scripts/MusicaFondo.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
-
+/* 
+ * Esta clase se encarga de reproducir la música de fondo en todas las escenas.
+ */
 public class MusicaFondo : MonoBehaviour
 {
+    // Esta función se llama cuando se inicia el juego y se asegura de que la música de fondo no se detenga al cambiar de escena
     void Awake()
     {
         DontDestroyOnLoad(gameObject);  // Hace que este objeto no se destruya al cargar una nueva escena
